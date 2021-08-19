06fef851d722197c362a7cbae6c5e86374cd83b9
shiken13 <43486453+shiken13@users.noreply.github.com>
2021-08-19 16:41:26 +0900

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -43,10 +43,10 @@ $(function () {
       // 開いている状態のwindow幅を取得し、レスポンシブ境目かどうか
       if ($(window).width() > spBreakPoint) {
         // 境目より大きいならばheaderの高さは620で固定
-        headerHeight = 550;
+        headerHeight = 50;
       } else {
         // 境目より狭ければレスポンシブの幅を適用する
-        headerHeight = 550;
+        headerHeight = 50;
       }
 
       // windowがスクロールされたときに以下関数が発動
@@ -90,54 +90,3 @@ $(function(){
 });
 
 
-
-
-
-/* dropアニメーション */
-
-;(function($) {
-    $.fn.letterDrop = function() {
-      // Chainability
-      return this.each( function() { 
-      var obj = $( this );
-      var drop = {
-        arr : obj.text().split( '' ),
-        range : {
-          min : 1,
-          max : 9
-        },
-        styles : function() {
-          var dropDelays = '\n', addCSS;
-           for ( i = this.range.min; i <= this.range.max; i++ ) {
-             dropDelays += '.ld' + i + ' { animation-delay: 1.' + i + 's; }\n';  
-           }
-            addCSS = $( '<style>' + dropDelays + '</style>' );
-            $( 'head' ).append( addCSS );
-        },
-        main : function() {
-          var dp = 0;
-          obj.text( '' );
-          $.each( this.arr, function( index, value ) {
-            dp = dp.randomInt( drop.range.min, drop.range.max );
-            if ( value === ' ' )
-              value = '&nbsp'; //Add spaces
-              obj.append( '<span class="letterDrop ld' + dp + '">' + value + '</span>' );
-          });
-        }
-      };
-      Number.prototype.randomInt = function ( min, max ) {
-        return Math.floor( Math.random() * ( max - min + 1 ) + min );
-      };
-      // Create styles
-      drop.styles();
-        // Initialise
-        drop.main();
-      });
-    
-    };
-}(jQuery));
-    
-    // USAGE
-    $( 'h1' ).letterDrop();
-    $( '.ken' ).letterDrop();
-    
